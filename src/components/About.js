c193c57d43ec6cff20e5114742e6b84055d9238a
Ian Holsteen <ianholsteen@gmail.com>
2022-11-21 21:48:25 -0800

    commit tree 9dc396ae4bd597d477ad0fb4bb7bb1d372a46aa5

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links"
+// import user from "../data/user";
 
-function About() {
+function About(props) {
+  console.log(props.github)
+  if (!props.bio) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return "null";
+  } else {
+    // show published content
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
-  );
+  );}
 }
 
 export default About;
